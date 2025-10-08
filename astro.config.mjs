9674603befb75c966fdb5b4aa3059c2e52e078cb
biojet-enviro <i@biojet.tech>
2025-10-08 16:51:23 +0800

    try deploy #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://biojet-enviro.github.io',
+    base: '/Biojet-Website',   // ← 关键
+});
